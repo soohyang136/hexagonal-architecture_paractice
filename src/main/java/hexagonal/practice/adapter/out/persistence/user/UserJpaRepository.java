31e5dc74535f5a688a7341aea51d9fb4ade6b3e8
soohyang136 <jsh515228@gmail.com>
2023-07-11 17:15:40 +0900

    user persistence

---
+++ src/main/java/hexagonal/practice/adapter/out/persistence/user/UserJpaRepository.java
@@ -0,0 +1,8 @@
+package hexagonal.practice.adapter.out.persistence.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<UserJpaEntity, Long> {
+}
