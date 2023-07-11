b8566d8a67065be8d068db2efa495a53b06ebe74
soohyang136 <jsh515228@gmail.com>
2023-07-11 16:30:25 +0900

    ADD :: user domain model

--- src/main/java/hexagonal/practice/adapter/out/persistence/user/UserJpaEntity.java
+++ src/main/java/hexagonal/practice/adapter/out/persistence/user/UserJpaEntity.java
@@ -1,13 +1,23 @@
 package hexagonal.practice.adapter.out.persistence.user;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserJpaEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(unique = true)
+    String userId;
+
+    String password;
 
     String name;
 }
---
+++ src/main/java/hexagonal/practice/domain/user/User.java
@@ -0,0 +1,8 @@
+package hexagonal.practice.domain.user;
+
+public class User {
+    Long id;
+    String userId;
+    String password;
+    String name;
+}
